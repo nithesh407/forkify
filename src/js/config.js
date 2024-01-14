eaaa74c8a4c77bab962e615d7a24ff7e03f7427b
nithesh407 <nitheshravikumar13631@gmail.com>
2024-01-14 21:50:10 +0530

    recipe application

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '79bed66e-dc5d-4a74-bcbf-0f0d6a2bb0ad';
+export const MODAL_CLOSE_SEC = 2.5;
